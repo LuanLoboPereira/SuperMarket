3b7e7b0f48238561b430d5c352dffc6820fb3750
LuanLoboPereira <luan.pereira@crescevendas.com>
2026-03-14 21:53:34 -0300

    Animação de bolinha para o carregamento

--- app/spiner.rb
+++ app/spiner.rb
@@ -2,10 +2,10 @@ require 'tty-spinner'
 module  Spinner 
   class SpinnerProgress 
     def progress
-      spinner = TTY::Spinner.new("[:spinner] Carregando lista de produtos...", format: :pulse_2)
-      spinner.auto_spin # Automatic animation with default interval
-      sleep(2) # Perform task
-      spinner.stop("Done!") # Stop animation
+      spinner = TTY::Spinner.new("[:spinner] Carregando lista de produtos...", format: :bouncing_ball)
+      spinner.auto_spin 
+      sleep(1.5) 
+      spinner.stop("\nProdutos Carregados") 
     end
 end 
 end
